caebdce59075fc19a92448065256bda87de8f288
jmwasenga <nsangu.jm@gmail.com>
2017-09-22 12:37:51 +0300

    modify Nav style

--- components/Nav.js
+++ components/Nav.js
@@ -6,25 +6,29 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 var Nav = () => {
   return (
 	   <AppBar
-            title="React Weather" style={{backgroundColor: '#dcdcdc'}}
+            title="React Weather" style={{backgroundColor: '#dcdcdc', width: '900px'}}
            >
 
-           <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', fontWeight: 'bold', margin: '10px', backgroundColor: '#dcdcdc', width: '350px'}}>
+           <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', position: 'relative', left: '-200px', fontWeight: 'bold', margin: '10px', width: '350px'}}>
 
 
-             <div style={{margin: '10px'}}>
+             <div style={{marginTop: '12px', marginLeft: '8px'}}>
                    <IndexLink to="/" activeClassName="active" >Get Weather</IndexLink>
 	     </div>
 
-             <div  style={{margin: '10px'}}>
+             <div  style={{marginTop: '12px', marginLeft: '8px'}}>
 		  <Link to="/about" activeClassName="active"  >About</Link>
 	     </div>
              
-             <div style={{margin: '10px'}}>
+             <div style={{marginTop: '12px', marginLeft: '8px'}}>
                    <Link to="/examples" activeClassName="active" >Examples</Link>	
      	      </div>
 
 	  </div>
+           <div style={{fontSize: '10px', marginTop: '20px', color: '#3949AB', fontWeight: 'bold'}}>
+     	       <p>by John MWasenga</p>
+		
+	   </div> 
 
 
 
